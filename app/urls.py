3f740721c074eff442bb1639fc445f36379eacbf
LucasRib52 <97406660+LucasRib52@users.noreply.github.com>
2025-04-09 20:25:38 -0300

    alterando url

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView  # 👈 importa isso
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/vendas/', permanent=False)),  # 👈 redireciona a raiz
     path('admin/', admin.site.urls),
     path('vendas/', include('venda.urls')),
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
-
-    # Inclui as rotas de autenticação padrões (login, logout, reset de senha etc.)
     path('accounts/', include('django.contrib.auth.urls')),
 ]
